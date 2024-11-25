838f4adc43246b5f7ba9ce570cacf66edb360c6a
howlil <muhammadulilabshar27@gmail.com>
2024-11-25 23:22:08 +0700

    change docker

--- tracking_nutrition_baby_api/run.py
+++ tracking_nutrition_baby_api/run.py
@@ -2,5 +2,9 @@ from app import create_app
 
 app = create_app()
 
+@app.route('/')
+def home():
+    return "Hello, Cloud Run!", 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
